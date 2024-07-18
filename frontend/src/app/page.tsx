1af18463a26316a1d6be722d7669d83a2816488b
Kelvin Chukwu <chukwu.kelvin@yahoo.com>
2024-07-17 21:48:03 -0400

    pagination in page url

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,10 @@
+const DEFAULT_PAGE = 1
+const DEFAULT_PAGE_SIZE = 10
 
-async function getCountries() {
-  const res = await fetch(`http://127.0.0.1:5000/countries/?page=${1}&page_size=${10}`)
+
+async function getCountries(page?: number , pageSize?: number) {
+  console.log("Page num: ",page)
+  const res = await fetch(`http://127.0.0.1:5000/countries/?page=${page ?? DEFAULT_PAGE}&page_size=${pageSize ?? DEFAULT_PAGE_SIZE}`)
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
  
@@ -8,13 +12,14 @@ async function getCountries() {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
   }
+  console.log(res.headers.get('x-pagination'))
  
   return res.json()
 }
 
-export default async function Home() {
-  const countries = await getCountries()
-  console.log(countries)
+export default async function Home({searchParams} : {searchParams?: {page?: number}}) {
+  const countries = await getCountries(searchParams?.page)
+  //console.log(countries)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
