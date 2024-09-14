4f01d8f0a70fa542bcc7ae25b5276a0938af2cbb
Kelvin Chukwu <chukwu.kelvin@yahoo.com>
2024-09-13 21:52:28 -0400

    successfully pass pagination state to table

--- frontend/src/app/countries/page.tsx
+++ frontend/src/app/countries/page.tsx
@@ -17,21 +17,34 @@ import Link from "next/link"
 
 
 const DEFAULT_PAGE = 1
-const DEFAULT_PAGE_SIZE = 10
+const DEFAULT_PAGE_SIZE = 15
+
+export type PaginationState = {
+  page?: number
+  total: number
+  totalPages: number
+  nextPage?: number
+  prevPage?: number
+  lastPage?: number
+}
+
+type CountryResponse = {
+  countries: Country[]
+  pagination: PaginationState
+}
 
 
-async function getCountries(page?: number, pageSize?: number): Promise<Country[]> {
-  console.log("Page num: ", page)
+async function getCountries(page?: number, pageSize?: number): Promise<CountryResponse> {
+  // TODO: use URL search params
   const res = await fetch(`http://127.0.0.1:5000/countries/?page=${page ?? DEFAULT_PAGE}&page_size=${pageSize ?? DEFAULT_PAGE_SIZE}`)
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
-  console.log(res.headers.get('x-pagination'))
-
+  const paginationRespone = JSON.parse(res.headers.get('x-pagination') ?? '{}')
   const resJSON = await res.json()
 
-  const countries = resJSON.map((country: { code: string; name: string; continent: string; population: number; surface_area: number; life_expectancy: number }) => {
+  const countries: Country[] = resJSON.map((country: { code: string; name: string; continent: string; population: number; surface_area: number; life_expectancy: number }) => {
     return {
       code: country.code,
       name: country.name,
@@ -41,14 +54,21 @@ async function getCountries(page?: number, pageSize?: number): Promise<Country[]
       lifeExpectancy: country.life_expectancy
     }
   })
-  console.log(countries)
-  return countries
+  return {
+    countries,
+    pagination: {
+      total: paginationRespone.total,
+      totalPages: paginationRespone.total_pages,
+      page: paginationRespone?.page,
+      nextPage: paginationRespone?.next_page,
+      prevPage: paginationRespone?.prev_page,
+      lastPage: paginationRespone?.last_page,
+    }
+  }
 }
 
 export default async function Countries({ searchParams }: { searchParams?: { page?: number } }) {
-  const countries = await getCountries(searchParams?.page)
-  //console.log(countries)
-
+  const countriesPagination = await getCountries(searchParams?.page)
   return (
     <main>
       <nav>
@@ -68,8 +88,8 @@ export default async function Countries({ searchParams }: { searchParams?: { pag
         </NavigationMenu>
       </nav>
 
-      <div className="flex flex-col items-center justify-between p-24 border-2 border-red-500 m-20">
-        <DataTable columns={columns} data={countries} />
+      <div className="flex flex-col items-center justify-between m-20">
+        <DataTable columns={columns} data={countriesPagination.countries} pagination={countriesPagination.pagination}/>
       </div>
     </main>
   );
