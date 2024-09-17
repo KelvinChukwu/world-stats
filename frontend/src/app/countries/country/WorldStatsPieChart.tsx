5d4de2580f183c5e792596a85dac7ebb9f979d8a
Kelvin Chukwu <chukwu.kelvin@yahoo.com>
2024-09-17 14:06:16 -0400

    extract chart into its own component with ability to pass in data

--- frontend/src/app/countries/country/WorldStatsPieChart.tsx
+++ frontend/src/app/countries/country/WorldStatsPieChart.tsx
@@ -11,49 +11,15 @@ import {
 
 export const description = "A pie chart with a legend"
 
-const chartData = [
-  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  { browser: "other", visitors: 90, fill: "var(--color-other)" },
-]
-
-const chartConfig = {
-  visitors: {
-    label: "Visitors",
-  },
-  chrome: {
-    label: "Chrome",
-    color: "hsl(var(--chart-1))",
-  },
-  safari: {
-    label: "Safari",
-    color: "hsl(var(--chart-2))",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "hsl(var(--chart-3))",
-  },
-  edge: {
-    label: "Edge",
-    color: "hsl(var(--chart-4))",
-  },
-  other: {
-    label: "Other",
-    color: "hsl(var(--chart-5))",
-  },
-} satisfies ChartConfig
-
-export function WorldStatsPieChart() {
+export function WorldStatsPieChart<TData>({chartData, chartConfig, dataKey, nameKey}: {chartData: TData[], chartConfig: ChartConfig, dataKey: string, nameKey: string}) {
   return (
       <ChartContainer
         config={chartConfig}
       >
         <PieChart>
-          <Pie data={chartData} dataKey="visitors" />
+          <Pie data={chartData} dataKey={dataKey} />
           <ChartLegend
-            content={<ChartLegendContent nameKey="browser" />}
+            content={<ChartLegendContent nameKey={nameKey} />}
             className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
           />
         </PieChart>
--- frontend/src/app/countries/country/page.tsx
+++ frontend/src/app/countries/country/page.tsx
@@ -17,6 +17,41 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { WorldStatsPieChart } from "./WorldStatsPieChart"
+import { ChartConfig } from "@/components/ui/chart"
+
+const chartData = [
+    { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+    { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+    { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+    { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+    { browser: "other", visitors: 90, fill: "var(--color-other)" },
+  ]
+  
+  const chartConfig = {
+    visitors: {
+      label: "Visitors",
+    },
+    chrome: {
+      label: "Chrome",
+      color: "hsl(var(--chart-1))",
+    },
+    safari: {
+      label: "Safari",
+      color: "hsl(var(--chart-2))",
+    },
+    firefox: {
+      label: "Firefox",
+      color: "hsl(var(--chart-3))",
+    },
+    edge: {
+      label: "Edge",
+      color: "hsl(var(--chart-4))",
+    },
+    other: {
+      label: "Other",
+      color: "hsl(var(--chart-5))",
+    },
+  } satisfies ChartConfig
 
 
 
@@ -160,7 +195,7 @@ export default async function CountryDetailedPage({ searchParams }: { searchPara
                             <CardTitle className="font-extrabold text-xl" >Languages</CardTitle> {/* TODO: make official languages bold and make a note of this with a question mark tooltip*/}
                         </CardHeader>
                         <CardContent>
-                            <WorldStatsPieChart/>
+                            <WorldStatsPieChart chartConfig={chartConfig} chartData={chartData} dataKey="visitors" nameKey="browser" />
                         </CardContent>
                     </Card>
                     <Button
