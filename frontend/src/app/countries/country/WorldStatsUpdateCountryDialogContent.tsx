5f0cc9dc0e605831b6b99b5a83dc508158500357
Kelvin Chukwu <chukwu.kelvin@yahoo.com>
2024-09-18 01:11:34 -0400

    adding form component from shadcn plus extracting content of dialog to its own client component to use as an interactive form.

---
+++ frontend/src/app/countries/country/WorldStatsUpdateCountryDialogContent.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+const wait = () => new Promise((resolve) => setTimeout(resolve, 2000));
+
+// TODO: MAKE SURE DIALOG CANNOT BE CLOSED WHILE REQUEST IS PENDING
+
+export function WorldStatsUpdateCountryDialogContent() {
+  return (
+    <DialogContent className="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>Edit profile</DialogTitle>
+        <DialogDescription>
+          Make changes to your profile here. Click save when you're done.
+        </DialogDescription>
+      </DialogHeader>
+      <form
+        onSubmit={ (event) => {
+          const foo = wait().then(() => {
+            console.log("submitted");
+          });
+          event.preventDefault();
+        }}
+      >
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  )
+}
--- frontend/src/app/countries/country/page.tsx
+++ frontend/src/app/countries/country/page.tsx
@@ -18,9 +18,10 @@ import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { WorldStatsBarChart } from "./WorldStatsBarChart"
 import { ChartConfig } from "@/components/ui/chart"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { WorldStatsUpdateCountryDialogContent } from "./WorldStatsUpdateCountryDialogContent"
 
 const chartConfig = {
     language: {
@@ -193,31 +194,7 @@ export default async function CountryDetailedPage({ searchParams }: { searchPara
                                 Edit
                             </Button>
                         </DialogTrigger>
-                        <DialogContent className="sm:max-w-[425px]">
-                            <DialogHeader>
-                                <DialogTitle>Edit profile</DialogTitle>
-                                <DialogDescription>
-                                    Make changes to your profile here. Click save when you're done.
-                                </DialogDescription>
-                            </DialogHeader>
-                            <div className="grid gap-4 py-4">
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="name" className="text-right">
-                                        Name
-                                    </Label>
-                                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
-                                </div>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="username" className="text-right">
-                                        Username
-                                    </Label>
-                                    <Input id="username" value="@peduarte" className="col-span-3" />
-                                </div>
-                            </div>
-                            <DialogFooter>
-                                <Button type="submit">Save changes</Button>
-                            </DialogFooter>
-                        </DialogContent>
+                        <WorldStatsUpdateCountryDialogContent />
                     </Dialog>
                 </div>
             </div>
