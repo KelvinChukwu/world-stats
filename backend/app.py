eca05323cb10feafd17b6d15c722f7ea4f6f5e65
Kelvin Chukwu <chukwu.kelvin@yahoo.com>
2025-03-17 23:54:05 -0400

    simple api endpoint for querying countries with graphql -> next step add countrylanguage

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,16 @@ from dotenv import dotenv_values
 from flask_smorest import Api, Blueprint, abort, Page
 from sqlalchemy import select
 from models import db, City, Country, Continent
-from schemas import CitySchema, CountryQueryArgsSchema, ma, CountrySchema, CityUpdateArgsSchema, CountryUpdateArgsSchema
+from schemas import (
+    CitySchema,
+    CountryQueryArgsSchema,
+    ma,
+    CountrySchema,
+    CityUpdateArgsSchema,
+    CountryUpdateArgsSchema,
+)
+from graphql_models import db as gdb, schema
+from graphql_server.flask import GraphQLView
 
 
 secrets = dotenv_values(".env")
@@ -13,34 +22,41 @@ secrets = dotenv_values(".env")
 
 app = Flask(__name__)
 
+
 class APIConfig:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
     DATABASE_URI = secrets["DATABASE_URI"]
     MYSQL_PASSWORD = secrets["MYSQL_PASSWORD"]
-    MYSQL_DB = 'world'
-    MYSQL_CURSORCLASS = 'DictCursor'
-    API_TITLE = 'WORLD STATS API'
-    API_VERSION = 'v1'
-    OPENAPI_VERSION = '3.1.0'
+    MYSQL_DB = "world"
+    MYSQL_CURSORCLASS = "DictCursor"
+    API_TITLE = "WORLD STATS API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.1.0"
     OPENAPI_URL_PREFIX = "/"
-    OPENAPI_SWAGGER_UI_PATH = '/docs'
-    OPENAPI_SWAGGER_UI_URL = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
+    OPENAPI_SWAGGER_UI_PATH = "/docs"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     SQLALCHEMY_DATABASE_URI = f"{DATABASE_URI}"
-    SQLALCHEMY_ENGINE_OPTIONS = {
-    'pool_recycle': 295
-    }
-    
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_recycle": 295}
+
 
 app.config.from_object(APIConfig)
 mysql = MySQL(app)
 api = Api(app)
 
-db.init_app(app)
+# db.init_app(app)
+gdb.init_app(app)
 ma.init_app(app)
 
-countries_blp = Blueprint("countries", "countries",url_prefix = "/countries", description = "Operations on countries")
-cities_blp = Blueprint("cities", "cities",url_prefix = "/cities", description = "Operations on cities")
+countries_blp = Blueprint(
+    "countries",
+    "countries",
+    url_prefix="/countries",
+    description="Operations on countries",
+)
+cities_blp = Blueprint(
+    "cities", "cities", url_prefix="/cities", description="Operations on cities"
+)
 
 countries_schema = CountrySchema(many=True)
 country_schema = CountrySchema()
@@ -48,109 +64,148 @@ country_schema = CountrySchema()
 city_schema = CitySchema()
 cities_schema = CitySchema(many=True)
 
+
 # Routes
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
+
+
+app.add_url_rule(
+    "/graphql",
+    view_func=GraphQLView.as_view(
+        "graphql",
+        schema=schema,
+        graphiql=True,
+    ),
+)
+
 
 @countries_blp.route("/")
 class Countries(MethodView):
     @countries_blp.arguments(CountryQueryArgsSchema, location="query")
-    @countries_blp.response(200,countries_schema)
+    @countries_blp.response(200, countries_schema)
     @countries_blp.paginate()
-    def get (self, queryArgs, pagination_parameters:Page):
+    def get(self, queryArgs, pagination_parameters: Page):
         """List countries"""
         search_query = select(Country).order_by(Country.name)
         if "name_contains" in queryArgs:
-            search_query = search_query.where(Country.name.icontains(queryArgs.get("name_contains")))
-        if  "continent" in queryArgs:
-            search_query = search_query.where(Country.continent == queryArgs.get("continent").value)
+            search_query = search_query.where(
+                Country.name.icontains(queryArgs.get("name_contains"))
+            )
+        if "continent" in queryArgs:
+            search_query = search_query.where(
+                Country.continent == queryArgs.get("continent").value
+            )
         if "population_min" in queryArgs:
-            search_query = search_query.where(Country.population >= queryArgs.get("population_min"))
+            search_query = search_query.where(
+                Country.population >= queryArgs.get("population_min")
+            )
         if "population_max" in queryArgs:
-            search_query = search_query.where(Country.population <= queryArgs.get("population_max"))
+            search_query = search_query.where(
+                Country.population <= queryArgs.get("population_max")
+            )
         if "life_expectancy_min" in queryArgs:
-            search_query = search_query.where(Country.life_expectancy >= queryArgs.get("life_expectancy_min"))
+            search_query = search_query.where(
+                Country.life_expectancy >= queryArgs.get("life_expectancy_min")
+            )
         if "life_expectancy_max" in queryArgs:
-            search_query = search_query.where(Country.life_expectancy <= queryArgs.get("life_expectancy_max"))
+            search_query = search_query.where(
+                Country.life_expectancy <= queryArgs.get("life_expectancy_max")
+            )
         if "surface_area_min" in queryArgs:
-            search_query = search_query.where(Country.surface_area >= queryArgs.get("surface_area_min"))
-        if  "surface_area_max" in queryArgs:
-            search_query = search_query.where(Country.surface_area <= queryArgs.get("surface_area_max"))
-        countries_pagination = db.paginate(search_query,page=pagination_parameters.page, per_page=pagination_parameters.page_size)
+            search_query = search_query.where(
+                Country.surface_area >= queryArgs.get("surface_area_min")
+            )
+        if "surface_area_max" in queryArgs:
+            search_query = search_query.where(
+                Country.surface_area <= queryArgs.get("surface_area_max")
+            )
+        countries_pagination = db.paginate(
+            search_query,
+            page=pagination_parameters.page,
+            per_page=pagination_parameters.page_size,
+        )
         pagination_parameters.item_count = countries_pagination.total
         return countries_pagination.items
-     
+
+
 @countries_blp.route("/<country_code>")
 class CountriesByCode(MethodView):
-    @countries_blp.response(200,country_schema)
-    def get (self, country_code: str):
+    @countries_blp.response(200, country_schema)
+    def get(self, country_code: str):
         """Return Country based on code"""
-        country = db.session.execute(select(Country).filter_by(code = country_code)).scalar()
+        country = db.session.execute(
+            select(Country).filter_by(code=country_code)
+        ).scalar()
         if country:
             return country
         else:
-            abort(404,message ="Country not found")
-    
+            abort(404, message="Country not found")
+
     @cities_blp.arguments(CountryUpdateArgsSchema)
-    @cities_blp.response(200,country_schema)
+    @cities_blp.response(200, country_schema)
     def patch(self, update_data, country_code):
         """Update existing country"""
-        country = db.session.execute(select(Country).filter_by(code = country_code)).scalar()
+        country = db.session.execute(
+            select(Country).filter_by(code=country_code)
+        ).scalar()
         if country:
             if "population" in update_data and update_data["population"]:
                 country.population = update_data["population"]
             if "life_expectancy" in update_data and update_data["life_expectancy"]:
                 country.life_expectancy = update_data["life_expectancy"]
-            if "head_of_state" in update_data and  update_data["head_of_state"]:
+            if "head_of_state" in update_data and update_data["head_of_state"]:
                 country.head_of_state = update_data["head_of_state"]
             if "government_form" in update_data and update_data["government_form"]:
                 country.government_form = update_data["government_form"]
             db.session.commit()
         else:
-            abort(404,message ="Country not found")
-               
+            abort(404, message="Country not found")
+
+
 @cities_blp.route("/")
 class Cities(MethodView):
-    @cities_blp.response(200,cities_schema)
+    @cities_blp.response(200, cities_schema)
     @cities_blp.paginate()
-    def get (self, pagination_parameters):
+    def get(self, pagination_parameters):
         """List cities"""
-        cities_pagination = db.paginate(select(City).order_by(City.name),page=pagination_parameters.page, per_page=pagination_parameters.page_size)
+        cities_pagination = db.paginate(
+            select(City).order_by(City.name),
+            page=pagination_parameters.page,
+            per_page=pagination_parameters.page_size,
+        )
         pagination_parameters.item_count = cities_pagination.total
         print(pagination_parameters)
         return cities_pagination.items
 
+
 @cities_blp.route("/<id>")
 class CitiesByCode(MethodView):
-    @cities_blp.response(200,city_schema)
-    def get (self, id: int):
+    @cities_blp.response(200, city_schema)
+    def get(self, id: int):
         """Return city based on code"""
-        city = db.session.execute(select(City).filter_by(id = id)).scalar()
+        city = db.session.execute(select(City).filter_by(id=id)).scalar()
         if city:
             return city
         else:
-            abort(404,message ="City not found")
-    
+            abort(404, message="City not found")
+
     @cities_blp.arguments(CityUpdateArgsSchema)
-    @cities_blp.response(200,city_schema)
+    @cities_blp.response(200, city_schema)
     def patch(self, update_data, id: int):
         """Update existing city"""
-        city = db.session.execute(select(City).filter_by(id = id)).scalar()
+        city = db.session.execute(select(City).filter_by(id=id)).scalar()
         if city:
             city.population = update_data["population"]
             db.session.commit()
         else:
-            abort(404,message ="City not found")
-    
+            abort(404, message="City not found")
 
 
 api.register_blueprint(countries_blp)
 api.register_blueprint(cities_blp)
 
 # Run
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-
---
+++ backend/graphql_models.py
@@ -0,0 +1,99 @@
+import enum
+
+from typing import List
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, ForeignKey, Enum
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.associationproxy import AssociationProxy
+import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from graphene_sqlalchemy.types import ORMField
+
+db = SQLAlchemy()
+db.Model.query = db.session.query_property()
+
+class Continent (enum.Enum):
+    ASIA = 'Asia'
+    EUROPE = 'Europe'
+    NORTH_AMERICA = 'North_America'
+    AFRICA = 'Africa'
+    OCEANIA = 'Oceania'
+    ANTARCTICA = 'Antarctica'
+    SOUTH_AMERICA = 'South_America'
+
+class IsOfficialLanguage (enum.Enum):
+    T = 'T'
+    F = 'F'
+
+class CountryModel(db.Model):
+    __tablename__ = "country"
+    code: Mapped[str] = mapped_column(primary_key=True, type_= String(3))
+    code2: Mapped[str] = mapped_column(String(2))
+    capital_id: Mapped[int] = mapped_column(ForeignKey("city.id"),name="capital")
+    capital: Mapped["CityModel"] = relationship( primaryjoin="CountryModel.capital_id == CityModel.id")
+    languages: Mapped[List["CountryLanguageModel"]] = relationship(primaryjoin="CountryModel.code == CountryLanguageModel.country_code")
+    name: Mapped[str] = mapped_column(unique=True)
+    continent: Mapped[str] #= mapped_column(Enum('Asia','Europe', 'North_America', 'Africa','Oceania','Antarctica','South_America', name="country_continent"))
+    region: Mapped[str]
+    surface_area: Mapped[float]
+    indep_year: Mapped[int]
+    population: Mapped[int]
+    life_expectancy: Mapped[float]
+    gnp: Mapped[float]
+    local_name: Mapped[str]
+    government_form: Mapped[str]
+    head_of_state: Mapped[str]
+
+class CityModel (db.Model):
+    __tablename__ = "city"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    country_code: Mapped[str] = mapped_column(ForeignKey(CountryModel.code))
+    country: Mapped[CountryModel] = relationship(primaryjoin="CityModel.country_code == CountryModel.code")
+    country_name: AssociationProxy[str] = association_proxy("country", "name")
+    district: Mapped[str]
+    population: Mapped[int]
+
+class CountryLanguageModel (db.Model):
+    __tablename__ = "country_language"
+    language: Mapped[str] = mapped_column(primary_key=True)
+    country_code: Mapped[str] = mapped_column(ForeignKey(CountryModel.code), primary_key=True)
+    is_official: Mapped[IsOfficialLanguage] = mapped_column(Enum('T','F', name="country_language_is_official"))
+    percentage: Mapped[float]
+
+
+class Country(SQLAlchemyObjectType):
+    class Meta:
+        model = CountryModel
+        # use `only_fields` to only expose specific fields ie "name"
+        # only_fields = ("name",)
+        # use `exclude_fields` to exclude specific fields ie "last_name"
+        # exclude_fields = ("last_name",)
+
+class City(SQLAlchemyObjectType):
+    class Meta:
+        model = CityModel
+        # use `only_fields` to only expose specific fields ie "name"
+        # only_fields = ("name",)
+        # use `exclude_fields` to exclude specific fields ie "last_name"
+        # exclude_fields = ("last_name",)
+  
+
+class Query(graphene.ObjectType):
+    countries = graphene.List(Country)
+    # cities  = graphene.List(City)
+
+    def resolve_countries(self, info):
+        query = Country.get_query(info)
+        return query.all()
+"""     def resolve_cities(self, info):
+        query = City.get_query(info)
+        return query.all() """
+
+schema = graphene.Schema(query=Query)
+
+
+
+
+
