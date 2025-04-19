a8971df81d11d63215d63e93f743e61479d19cde
Kelvin Chukwu <chukwu.kelvin@yahoo.com>
2025-04-19 14:52:08 -0400

    skeleton for login/logout

--- frontend/src/app/world-stats-nav.tsx
+++ frontend/src/app/world-stats-nav.tsx
@@ -1,5 +1,47 @@
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import * as React from "react"
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import Link from "next/link";
+
+function ListItem({
+    title,
+    children,
+    href,
+    ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+    return (
+        <li {...props}>
+            <NavigationMenuLink asChild>
+                <Link href={href}>
+                    <div className="text-sm leading-none font-medium">{title}</div>
+                    <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+                        {children}
+                    </p>
+                </Link>
+            </NavigationMenuLink>
+        </li>
+    )
+}
+
+function LoginItem() {
+    return (
+        <NavigationMenuItem>
+            <NavigationMenuTrigger>Logged In As</NavigationMenuTrigger>
+            <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4">
+
+                    <ListItem
+                        key={"logout"}
+                        title={"Logout"}
+                        href={"#"}
+                    >
+                    </ListItem>
+                </ul>
+            </NavigationMenuContent>
+        </NavigationMenuItem>
+    )
+}
+
+
 
 export function WorldStatsNav() {
 
@@ -28,6 +70,7 @@ export function WorldStatsNav() {
                                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>Admin Login</NavigationMenuLink>
                                 </Link>
                             </NavigationMenuItem>
+                            <LoginItem />
                         </NavigationMenuList>
                     </NavigationMenu>
                 </nav>
